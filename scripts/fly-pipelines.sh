5ad27c93dd50f1f1747ba7204dab39eac0125e60
Brian Kirkland <bkirkland@pivotal.io>
2020-01-08 11:54:12 -0500

    bump pas to 2.7, add scg

--- scripts/fly-pipelines.sh
+++ scripts/fly-pipelines.sh
@@ -11,7 +11,7 @@ fly -t kirklab-control set-pipeline -p upgrade-pcc -c ../pipelines/upgrade-pcc.y
 fly -t kirklab-control set-pipeline -p upgrade-harbor -c ../pipelines/upgrade-harbor.yml -v foundation=kirklab -v product_slug=harbor-container-registry -v product_shortname=harbor -v product_version="^1\.10\..*$"
 # fly -t kirklab-control set-pipeline -p upgrade-healthwatch -c ../pipelines/upgrade-healthwatch.yml -v foundation=kirklab -v product_slug=p-healthwatch -v product_shortname=healthwatch -v product_version="^1\.6\..*$"
 fly -t kirklab-control set-pipeline -p upgrade-mysql -c ../pipelines/upgrade-mysql.yml -v foundation=kirklab -v product_slug=pivotal-mysql -v product_shortname=mysql -v product_version="^2\.7\..*$"
-fly -t kirklab-control set-pipeline -p upgrade-pas-srt -c ../pipelines/upgrade-pas-srt.yml -v foundation=kirklab -v product_slug=cf -v product_shortname=pas-srt -v product_version="^2\.6\..*$"
+fly -t kirklab-control set-pipeline -p upgrade-pas-srt -c ../pipelines/upgrade-pas-srt.yml -v foundation=kirklab -v product_slug=cf -v product_shortname=pas-srt -v product_version="^2\.7\..*$"
 fly -t kirklab-control set-pipeline -p upgrade-pks -c ../pipelines/upgrade-pks.yml -v foundation=kirklab -v product_slug=pivotal-container-service -v product_shortname=pks -v product_version="^1\.6\..*$"
 fly -t kirklab-control set-pipeline -p upgrade-control-pks -c ../pipelines/upgrade-control-pks.yml -v foundation=kirklab-control -v product_slug=pivotal-container-service -v product_shortname=pks -v product_version="^1\.6\..*$"
 fly -t kirklab-control set-pipeline -p upgrade-rabbitmq -c ../pipelines/upgrade-rabbitmq.yml -v foundation=kirklab -v product_slug=p-rabbitmq -v product_shortname=rabbitmq -v product_version="^1\.18\..*$"
@@ -21,6 +21,7 @@ fly -t kirklab-control set-pipeline -p upgrade-credhub -c ../pipelines/upgrade-s
 # fly -t kirklab-control set-pipeline -p upgrade-scs -c ../pipelines/upgrade-scs.yml -v foundation=kirklab -v product_slug=p-spring-cloud-services -v product_shortname=scs -v product_version="^2\.0\..*$"
 fly -t kirklab-control set-pipeline -p upgrade-scs3 -c ../pipelines/upgrade-scs3.yml -v foundation=kirklab -v product_slug=p-spring-cloud-services -v product_slug2=p_spring-cloud-services -v product_shortname=scs3 -v product_version="^3\.1\..*$"
 fly -t kirklab-control set-pipeline -p upgrade-ncp -c ../pipelines/upgrade-ncp.yml -v foundation=kirklab -v product_slug=VMware-NSX-T -v product_shortname=ncp -v product_version="^2\.5\..*$"
+fly -t kirklab-control set-pipeline -p upgrade-scg -c ../pipelines/upgrade-scs3.yml -v foundation=kirklab -v product_slug=p_spring-cloud-gateway-service -v product_slug2=p_spring-cloud-gateway-service -v product_shortname=scg -v product_version="^1\.0\..*$"
 
 # Apply Changes
 fly -t kirklab-control set-pipeline -p apply-changes -c ../pipelines/apply-changes.yml -v foundation=kirklab
@@ -54,6 +55,7 @@ fly -t kirklab-control unpause-pipeline -p upgrade-credhub
 # fly -t kirklab-control unpause-pipeline -p upgrade-scs
 fly -t kirklab-control unpause-pipeline -p upgrade-scs3
 fly -t kirklab-control unpause-pipeline -p upgrade-ncp
+fly -t kirklab-control unpause-pipeline -p upgrade-scg
 fly -t kirklab-control unpause-pipeline -p apply-changes
 fly -t kirklab-control unpause-pipeline -p control-apply-changes
 fly -t kirklab-control unpause-pipeline -p export-inst
